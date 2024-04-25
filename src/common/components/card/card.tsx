0788b62190a04230c3bf41b675adb87a8f80d147
Pavel Osipau <pavelosipov777@gmail.com>
2024-04-26 02:03:35 +0300

    add comments

--- src/common/components/card/card.tsx
+++ src/common/components/card/card.tsx
@@ -1,7 +1,9 @@
 import type { CardProps } from "./types";
 import "./styles.css";
 
+// можно сделать дженериком
 export const Card = ({ item }: CardProps) => {
+  // поскольку в запросе эти поля null, то сделал подобное решение, но если вдруг поля будут заполнены, то всё отобразиться как надо
   const title = item.title || `title for ${item.nation.name} ${item.level} level ${item.type.name}`;
   const description = item.description || `description for ${item.nation.name} ${item.level} level ${item.type.name}`;
 
